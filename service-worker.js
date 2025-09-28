e83a6100db1e8e8650902cd9bb7eefd54f736026
Jakob Beith Severinsen <jakbeisev@gmail.com>
2025-09-28 14:02:25 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,7 @@
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('regn-cache').then(function(cache) {
-      return cache.addAll([
-        './index.html',
-        './favicon.png',
-        './manifest.json'
-      ]);
-    })
-  );
+self.addEventListener('install', event => {
+  console.log('Service worker installed');
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
